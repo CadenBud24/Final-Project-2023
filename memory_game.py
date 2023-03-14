a623cca3d441e41cc7ea04de5e1bb3b9e6741f04
AidenRBaker <battlecat1430@gmail.com>
2023-03-14 11:37:33 -0700

    bla bla bla i like men

--- memory_game.py
+++ memory_game.py
@@ -2,10 +2,12 @@
 import pygame
 import random
 
-#def
+
 def display(text, x, y, size):
     set_my_font = pygame.font.SysFont("timesnewroman", 30)
     textSurface = set_my_font.render(text, True, "#000000")
 
 screen = pygame.display.set_mode((600,800))
 pygame.display.set_caption("Memory Game")
+
+display()

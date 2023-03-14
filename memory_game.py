71f5f5da84744b1ba3027fb2c458f24a01ca97ca
AidenRBaker <battlecat1430@gmail.com>
2023-03-14 11:42:17 -0700

    added game loop

--- memory_game.py
+++ memory_game.py
@@ -7,7 +7,22 @@ def display(text, x, y, size):
     set_my_font = pygame.font.SysFont("timesnewroman", 30)
     textSurface = set_my_font.render(text, True, "#000000")
 
+
 screen = pygame.display.set_mode((600,800))
 pygame.display.set_caption("Memory Game")
 
-display()
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                pass
+
+
+
+    pygame.display.flip()
+
+pygame.quit()

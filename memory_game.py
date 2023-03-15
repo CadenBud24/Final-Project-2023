dfd3537177b6e44ab9d6a9f008450ffada37724f
CadenBud24 <cadembudd@gmail.com>
2023-03-15 12:05:10 -0700

    re did and added images hopefully fingers crossed

--- memory_game.py
+++ memory_game.py
@@ -1,39 +1,29 @@
-#imports 
 import pygame
-import random
-
-
-def display(text, x, y, size):
-    set_my_font = pygame.font.SysFont("timesnewroman", 30)
-    textSurface = set_my_font.render(text, True, "#0000FF")
-
-
-screen = pygame.display.set_mode((800,800))
-pygame.display.set_caption("Memory Game")
-
+background_colour = (200,200,200)
+(width, height) = (800, 600)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Memory Game')
+screen.fill(background_colour)
+pygame.display.flip()
 running = True
 while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                pass
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
 
 
-
-    pygame.display.flip()
-
-pygame.quit()
-blue_image = pygame.image.load("Python/Repositories/Final-Project-2023/big_blue_2.png")
-blue_image = pygame.transform.flip(blue_image, True, False)
+blue_image = pygame.image.load("big_blue_2.png")
 blue_image = pygame.transform.scale(blue_image, (200,200))
-def blue_picture(x,y):
-   screen.blit(blue_image, (200,100))
 
+red_image = pygame.image.load("big_red.png")
+red_image = pygame.transform.scale(red_image, (200,200))
 
+yellow_image = pygame.image.load("big_yellow.png")
+yellow_image = pygame.transform.scale(yellow_image, (200,200))
 
-blue_picture()
-random.randint(1,4)
+green_image = pygame.image.load("big_green.png")
+green_image = pygame.transform.scale(green_image, (200,200))
+
+purple_image = pygame.image.load("big_purple.png")
+purple_image = pygame.transform.scale(purple_image, (200,200))
 

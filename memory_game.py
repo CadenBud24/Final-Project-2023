2e8cf5f30007570a24a40fa64c867ce128935f24
CadenBud24 <cadembudd@gmail.com>
2023-03-15 11:30:46 -0700

    changed some things hahah

--- memory_game.py
+++ memory_game.py
@@ -5,10 +5,10 @@ import random
 
 def display(text, x, y, size):
     set_my_font = pygame.font.SysFont("timesnewroman", 30)
-    textSurface = set_my_font.render(text, True, "#000000")
+    textSurface = set_my_font.render(text, True, "#0000FF")
 
 
-screen = pygame.display.set_mode((600,800))
+screen = pygame.display.set_mode((800,800))
 pygame.display.set_caption("Memory Game")
 
 running = True
@@ -26,7 +26,14 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
-blue_image = pygame.image.load("assets/lizard.png")
+blue_image = pygame.image.load("Python/Repositories/Final-Project-2023/big_blue_2.png")
 blue_image = pygame.transform.flip(blue_image, True, False)
 blue_image = pygame.transform.scale(blue_image, (200,200))
+def blue_picture(x,y):
+   screen.blit(blue_image, (200,100))
+
+
+
+blue_picture()
 random.randint(1,4)
+
